72d33f1f2df0c8dfa162282521e76a849b7d8093
Anastasiaa Senina <miss.s-anas.itmm@yandex.ru>
2016-11-27 16:19:36 +0400

    realization of fanction and queue

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,11 @@
 #include "queue.h"
-#include <iostream>
 
 int main() {
+const int N=5;
+  int mas[N] = {1,2,3,4};
+  const int window=3;
+  double resmas[N-N%window];
+  SumOfAverageValue(N,mas,window,resmas);
+  for (int i = 0; i<(N - N%window); i++)
+   printf("%lf\n", resmas[i]);
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,7 +1,55 @@
 #include <gtest/gtest.h>
 #include "queue.h"
 
+TEST(queue, can_get_front_element) {
+  Queue q;
+  q.push(2);
+  q.push(3);
+  q.push(5);
+  EXPECT_EQ(5, q.front());
+}
 
+TEST(queue, can_not_get_front_element_if_queue_is_empty) {
+  Queue q;
+  ASSERT_ANY_THROW(q.front());
+}
+
+TEST(queue, can_get_back_element) {
+  Queue q;
+  q.push(2);
+  q.push(3);
+  q.push(5);
+  EXPECT_EQ(2, q.back());
+}
+
+TEST(queue, can_not_get_back_element_if_queue_is_empty) {
+  Queue q;
+  ASSERT_ANY_THROW(q.back());
+}
+TEST(queue, can_pop_element_in_queue_isnt_empty) {
+  Queue q;
+  q.push(2);
+  q.push(3);
+  q.push(5);
+  q.pop();
+  EXPECT_EQ(3,q.back());
+}
+
+TEST(queue, can_not_pop_element_in_is_empty) {
+  Queue q;
+
+  ASSERT_ANY_THROW(q.pop());
+}
+
+TEST(queue, can_correct_size_is_empty_queue){
+  Queue q;
+  q.push(3);
+  EXPECT_FALSE(q.empty());
+}
+TEST(queue, can_correct_size_isnt_empty_queue){
+  Queue q;
+  EXPECT_TRUE(q.empty());
+}
 int main(int ac, char* av[])
 {
   testing::InitGoogleTest(&ac, av);
